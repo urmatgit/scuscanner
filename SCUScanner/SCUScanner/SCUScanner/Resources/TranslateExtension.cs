29b3cef5762cb2738cf1bd9698e5d53ae328318a
Urmat <urmat2g@gmail.com>
2018-01-05 01:37:04 +0300

    Convert to .net standart project

---
+++ SCUScanner/SCUScanner/SCUScanner/Resources/TranslateExtension.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Reflection;
+using System.Resources;
+using System.Text;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace SCUScanner.Resources
+{
+    [ContentProperty("Text")]
+    public class TranslateExtension : IMarkupExtension
+    {
+        private readonly CultureInfo _cultureInfo;
+        const string ResourceId = "LocalizeApp.Resource";
+        public TranslateExtension()
+        {
+            _cultureInfo = DependencyService.Get<Services.ILocalizeService>().GetCurrentCultureInfo();
+        }
+
+        public string Text { get; set; }
+
+        public object ProvideValue(IServiceProvider serviceProvider)
+        {
+            if (Text == null)
+            {
+                return null;
+            }
+            ResourceManager resmgr = new ResourceManager(ResourceId,
+                      typeof(TranslateExtension).GetTypeInfo().Assembly);
+            var translation = resmgr.GetString(Text, _cultureInfo);
+
+#if DEBUG
+            if (translation == null)
+            {
+                throw new ArgumentException(string.Format("Key {0} was not found for culture {1}.", Text, _cultureInfo));
+            }
+#endif
+            return translation;
+        }
+    }
+}
