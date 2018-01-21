b30ce9311cd20a7c8325924d0ff19a2a7809bd48
Urmat <urmat2g@gmail.com>
2018-01-21 12:18:24 +0300

    Connect devices and read charecteristic

--- SCUScanner/SCUScanner/SCUScanner/ViewModels/GattCharacteristicViewModel.cs
+++ SCUScanner/SCUScanner/SCUScanner/ViewModels/GattCharacteristicViewModel.cs
@@ -1,4 +1,5 @@
 ﻿using Plugin.BluetoothLE;
+using ReactiveUI;
 using System;
 using System.Collections.Generic;
 using System.Text;
@@ -11,6 +12,41 @@ namespace SCUScanner.ViewModels
         public GattCharacteristicViewModel(IGattCharacteristic characteristic)
         {
             Characteristic = characteristic;
+        }
+        public Guid Uuid => this.Characteristic.Uuid;
+        public Guid ServiceUuid => this.Characteristic.Service.Uuid;
+        public string Description => this.Characteristic.Description;
+        public string Properties => this.Characteristic.Properties.ToString();
+        public bool CanNotify => this.Characteristic.CanNotify();
+        string value;
+        public string Value
+        {
+            get => this.value;
+            private set => this.RaiseAndSetIfChanged(ref this.value, value);
+        }
+
+
+        bool notifying;
+        public bool IsNotifying
+        {
+            get => this.notifying;
+            private set => this.RaiseAndSetIfChanged(ref this.notifying, value);
+        }
+
+
+        bool valueAvailable;
+        public bool IsValueAvailable
+        {
+            get => this.valueAvailable;
+            private set => this.RaiseAndSetIfChanged(ref this.valueAvailable, value);
+        }
+
+
+        DateTime lastValue;
+        public DateTime LastValue
+        {
+            get => this.lastValue;
+            private set => this.RaiseAndSetIfChanged(ref this.lastValue, value);
         }
     }
 }
