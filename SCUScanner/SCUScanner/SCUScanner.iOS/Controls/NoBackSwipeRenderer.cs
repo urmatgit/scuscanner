e4bb1a2abfeb42c5dc4d6aa9eef031d59957b190
urmat <urmat2g@gmail.com>
2019-12-12 23:09:32 +0300

    -bug NoBackSwipe

---
+++ SCUScanner/SCUScanner/SCUScanner.iOS/Controls/NoBackSwipeRenderer.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using CentriClean.Controls;
+using Foundation;
+using UIKit;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.iOS;
+[assembly: ExportRenderer(typeof(ContentPage), typeof(NoBackSwipeRenderer))]
+namespace CentriClean.Controls
+{
+    
+    public class NoBackSwipeRenderer : PageRenderer
+    {
+        public override void ViewWillAppear(bool animated)
+        {
+            base.ViewWillAppear(animated);
+
+            if (ViewController != null && ViewController.NavigationController != null)
+                ViewController.NavigationController.InteractivePopGestureRecognizer.Enabled = false;
+        }
+    }
+}
